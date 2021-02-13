4b88f0a487672c08e5ef57b5c5fc6dc812dd25fc
sathishkumar <sathish.thangavel@solitontech.com>
2021-02-13 21:50:37 +0530

    Learning : javascript use strict
    Signed-off-by: sathishkumar <sathish.thangavel@solitontech.com>

---
+++ Basics/1_use_strict/use_strict.js
@@ -0,0 +1,12 @@
+"use strict";
+a = 10; //bad not allowed 
+var a = 10; //good allowed 
+console.log(a);
+
+
+/* Learning Tips
+1) use strict allows the broswer to work with modern way javascript
+2) one such example is above where we cannot above to assgin value to variable without declaration
+3) By default classes and modules comes with use strict directive
+4) use strict should be at the top of the file or top of function(only function level)
+*/
